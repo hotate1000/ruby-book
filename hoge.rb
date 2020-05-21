d58d9beccc6ce93746f94a558144cf080fa88759
hotate1000 <ooo.336.sss@gmail.com>
2020-05-21 21:10:34 +0900

    6章 正規表記

--- hoge.rb
+++ hoge.rb
@@ -1,3 +1,14 @@
-convert_length(1,'m','in')
-convert_length(15,'in','m')
-convert_length(35000,'ft','m')
+# text = '私の誕生日は1997年7月17日です。'
+# m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
+
+# puts m[1]
+# puts m[2]
+# puts m[3]
+# puts m[0,2]
+
+# puts m[:year]
+# puts m[:month]
+
+
+mm = '123 456 789'.scan(/d+/)
+puts mm
