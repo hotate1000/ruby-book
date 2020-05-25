9200b43b99da2ab685d37eda97f6f6ec6945db5a
hotate1000 <ooo.336.sss@gmail.com>
2020-05-25 14:03:16 +0900

    9章完了

--- lib/regxp_checker.rb
+++ lib/regxp_checker.rb
@@ -1,3 +1,24 @@
 print 'Text?:'
 text = gets.chomp
-puts text
+
+# print 'Pattern?:'
+# pattern = gets.chomp
+# regexp =Regexp.new(pattern)
+
+begin
+  print 'Pattern?:'
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue RegexpError => e
+  puts "Invalid pattern:#{e.message}"
+  retry
+end
+
+matches = text.scan(regexp)
+if matches.size > 0
+  puts "Matched:#{matches.join(',')}"
+else
+  puts "Nothing matched"
+end
+
+
